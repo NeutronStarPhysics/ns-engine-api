59b8d3581b83303f3ef595300d8bde0d6a9834bd
Rodrigo de Souza <rsouza01@gmail.com>
2024-06-24 07:51:15 +0000

    TOV modules added

--- src/ns_engine_api/algorithms/tov_solver/main.py
+++ src/ns_engine_api/algorithms/tov_solver/main.py
@@ -3,6 +3,10 @@ from ...domain.algorithm import Algorithm
 
 logger = logging.getLogger(__name__)
 
+def run(parameters):
+    tov_Solver = TOV_Solver()
+    tov_Solver.run()
+
 class TOV_Solver(Algorithm):
     def run(self):
         logger.debug(">>> TOV_Solver.run: " + type(self).__name__)
--- src/ns_engine_api/router.py
+++ src/ns_engine_api/router.py
@@ -2,13 +2,12 @@ import logging
 from fastapi import FastAPI
 
 from .domain.request import ExecutionRequest
-from .algorithms.tov_solver.main import TOV_Solver
+from .algorithms.tov_solver import main as tov_solver
 
 logger = logging.getLogger(__name__)
 
 def route_request(request: ExecutionRequest):
     logger.debug(">>> ROUTE_REQUEST: request" + str(request))
 
-    tov_Solver = TOV_Solver()
-    tov_Solver.run()
+    tov_solver.run(None)
     return request
